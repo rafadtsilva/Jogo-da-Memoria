b7a6c3726f87916a79948e410ebc078b42f33f20
Rafael Trindade <rafaeltrindade@outlook.com>
2021-07-24 15:39:38 -0300

    finalizado apresentation site

--- script.js
+++ script.js
@@ -2,15 +2,44 @@
 //PEGANDO HTMLS
 let imgScreen = document.getElementsByClassName('imagens');
 let img = document.getElementsByTagName('img');
+let avancar = document.getElementById('avancar');
+let voltar = document.getElementById('voltar');
+let imgAtual = document.getElementById('imgAtual');
+let circle = document.getElementsByClassName('circle')
+
+
+
+console.log(imgAtual)
 
 //DEFININDO VARIAVEIS
 let urlImg = ["assets/imagem1.jpg", "assets/imagem2.jpg", "assets/imagem3.jpg", "assets/imagem4.jpg"]
 let images = [];
+let imgNumber = 0;
+
+imgAtual.innerHTML = `${imgNumber+1} / ${urlImg.length}`;
+circle[imgNumber].style.backgroundColor = "black";
 
 urlImg.forEach((element, item) => {
   images[item] = new Image();
   images[item].src = element;
 });
 
-imgScreen[0].innerHTML = images[1].outerHTML;
+avancar.addEventListener('click', function () {
+  if (imgNumber < urlImg.length-1) {
+    imgNumber++;
+    imgScreen[0].innerHTML = images[imgNumber].outerHTML;
+    imgAtual.innerHTML = `${imgNumber+1} / ${urlImg.length}`;
+    circle[imgNumber-1].style.backgroundColor = "grey";
+    circle[imgNumber].style.backgroundColor = "black";
+  } 
+})
 
+voltar.addEventListener('click', function () {
+  if (imgNumber > 0) {
+    imgNumber--;
+    imgScreen[0].innerHTML = images[imgNumber].outerHTML;
+    imgAtual.innerHTML = `${imgNumber+1} / ${urlImg.length-1}`;
+    circle[imgNumber+1].style.backgroundColor = "grey";
+    circle[imgNumber].style.backgroundColor = "black";
+  }
+})
