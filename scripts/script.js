f6397a9f02b4ab2c2d7cfbab99cfcb144e6a7dd1
Rafael Trindade <rafaeltrindade@outlook.com>
2021-08-10 18:09:02 -0300

    bugFix: corrigido alguns bugs; feat: implementado a tela final e a reinicialização do jogo.

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ function startGame() {
 
 function initializeCards(cards) {
   let gameBoard = document.getElementById("gameBoard");
-  
+  gameBoard.innerHTML = "";
   game.cards.forEach(card => {
     
     let cardElement = document.createElement('div');
@@ -57,35 +57,37 @@ function createCardFace(face, card, element) {
 
 }
 
-
-
 function flipCard() {
 
   if(game.setCard(this.id)){
-  
+
     this.classList.add("flip");
 
-    if(game.checkMatch()) {
-
-      game.clearCards();
-
-    } else {
-
-      setTimeout(() => {
-
-        let firstCardView = document.getElementById(game.firstCard.id);
-        let secondCardView = document.getElementById(game.secondCard.id);
-  
-        firstCardView.classList.remove('flip');
-        secondCardView.classList.remove('flip');
-        game.clearCards();
-      
-      }, 1000);
-
-    };
+    if(game.secondCard) {
+      if(game.checkMatch()) {
+          game.clearCards();
+          if (game.checkGameOver()) {
+            let gameOverLayer = document.getElementById("gameOver")
+            gameOverLayer.style.display = 'flex';
+          }
+        } else {  
+          setTimeout(() => {
+            let firstCardView = document.getElementById(game.firstCard.id);
+            let secondCardView = document.getElementById(game.secondCard.id);
     
+            firstCardView.classList.remove('flip');
+            secondCardView.classList.remove('flip');
+            game.unflipCards();
+          }, 1000);
+        };
+    }
+
   }
 
+}
 
-
+function restart() {
+  startGame();
+  let gameOverLayer = document.getElementById("gameOver")
+  gameOverLayer.style.display = 'none';
 }
